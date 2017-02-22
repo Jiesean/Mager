1ef516e33f6bb14654794cb8fd646416504a20c0
Jiesean <out_frozen@163.com>
2017-02-22 19:00:12 +0800

    init UI

--- src/com/jiesean/mager/MainUI.java
+++ src/com/jiesean/mager/MainUI.java
@@ -6,7 +6,10 @@ import java.awt.Toolkit;
 import javax.naming.spi.DirObjectFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 import java.awt.Color;
+import javax.swing.JTextPane;
 
 public class MainUI extends JFrame{
 	private Dimension screenSize;
@@ -17,9 +20,21 @@ public class MainUI extends JFrame{
 		this.setSize((int)screenSize.getWidth(), (int)screenSize.getHeight());
 		getContentPane().setLayout(null);
 		
+		
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBackground(Color.PINK);
+		textPane.setLocation(0, 0);
+		textPane.setEditable(true);
+		textPane.setSize(this.getWidth()/2, this.getHeight());
+		
 		JPanel panel = new JPanel();
-		panel.setBackground(Color.GRAY);
+//		panel.setBackground(Color.GRAY);
 		panel.setBounds(0, 0, this.getWidth()/2, this.getHeight());
+		panel.setLayout(null);
+		
+		JScrollPane js=new JScrollPane(textPane);
+		panel.add(js);
 		getContentPane().add(panel);
 		
 		JPanel panel_1 = new JPanel();
